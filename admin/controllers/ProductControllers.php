8a9b00de4fbfdcf9498466421ca2f981aa56fd82
MacThom <macthom26121998@gmail.com>
2023-07-16 17:28:35 +0700

    commit x

--- admin/controllers/ProductControllers.php
+++ admin/controllers/ProductControllers.php
@@ -1,35 +1,66 @@
 <?php
 
 // require_once 'core/Database.php';
-require_once '../dao/pdo.php';
-require_once '../dao/product.php';
+require_once '../../model/product.php';
+require_once '../../../global.php';
 
-$products = new Product();
+// check_login();
 
-$action = isset($_GET['action']) ? $_GET['action'] : '';
+extract($_REQUEST);
+var_dump($_REQUEST) ;
+// if (exist_param("btn_insert")) {
+//     if ($giam_gia >= 0 && $giam_gia <= 100) {
+//         $ten_hh = convert_name($ten_hh);
+//         $file_name = save_file("up_hinh", "$IMAGE_DIR/products/");
+//         $hinh = $file_name ? $file_name : "";
+//         $ngay_nhap = $ngay_nhap ? $ngay_nhap : date_format(date_create(), 'Y-m-d');
+//         try {
+//             hang_hoa_insert($ten_hh, $don_gia, $giam_gia, $hinh, $ma_loai, $dac_biet, $ngay_nhap, $mo_ta);
+//             unset($ma_hh, $ten_hh, $don_gia, $giam_gia, $hinh, $ma_loai, $dac_biet, $ngay_nhap, $so_luot_xem, $mo_ta);
+//             $MESSAGE = "Thêm mới thành công!";
+//         } catch (Exception $exc) {
+//             $MESSAGE = "Thêm mới thất bại!";
+//         }
+//     } else {
+//         $MESSAGE = "Giảm giá chỉ từ 0-100%";
+//     }
+//     $VIEW_NAME = "hang-hoa/new.php";
+// } else if (exist_param("btn_update")) {
+//     if ($giam_gia >= 0 && $giam_gia <= 100) {
+//         $ten_hh = convert_name($ten_hh);
+//         $file_name = save_file("up_hinh", "$IMAGE_DIR/products/");
+//         $hinh = $file_name ? $file_name : $hinh;
+//         $ngay_nhap = $ngay_nhap ? $ngay_nhap : date_format(date_create(), 'Y-m-d');
+//         try {
+//             hang_hoa_update($ma_hh, $ten_hh, $don_gia, $giam_gia, $hinh, $ma_loai, $dac_biet, $ngay_nhap, $mo_ta);
+//             $MESSAGE = "Cập nhật thành công!";
+//         } catch (Exception $exc) {
+//             $MESSAGE = "Cập nhật thất bại!";
+//         }
+//     } else {
+//         $MESSAGE = "Giảm giá chỉ từ 0-100%";
+//     }
+//     $VIEW_NAME = "hang-hoa/edit.php";
+// } else if (exist_param("btn_delete")) {
+//     try {
+//         hang_hoa_delete($ma_hh);
+//         $MESSAGE = "Xóa thành công!";
+//     } catch (Exception $exc) {
+//         $MESSAGE = "Xóa thất bại!";
+//     }
+//     $items = hang_hoa_select_page();
+//     $VIEW_NAME = "hang-hoa/list.php";
+// } else if (exist_param("btn_edit")) {
+//     $item = hang_hoa_select_by_id($ma_hh);
+//     extract($item);
+//     $VIEW_NAME = "hang-hoa/edit.php";
+// } else if (exist_param("btn_list")) {
+//     $items = hang_hoa_select_page();
+//     $VIEW_NAME = "hang-hoa/list.php";
+// } else {
+    $VIEW_NAME = "view/product/default.php";
+// }
+require "../../layout.php";
 
-switch ($action) {
-    case 'add':
-        // Xử lý thêm sản phẩm
-        break;
-    case 'edit':
-        // Xử lý sửa sản phẩm
-        break;
-    case 'delete':
-        // Xử lý xóa sản phẩm
-        break;
-    case 'view':
-        // Xử lý xem chi tiết sản phẩm
-        break;
-    case 'search':
-        // Xử lý tìm kiếm sản phẩm
-        break;
-    case 'statistics':
-        // Xử lý thống kê sản phẩm
-        break;
-    default:
-        // Mặc định hiển thị danh sách sản phẩm
-        $products = $productDAO->findAll();
-        // Hiển thị view danh sách sản phẩm
-        break;
-}
+
+
--- admin/model/demo.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "Folder này chứa các thư mục xử lý của các chức năng cụ thể";
-
-?>
