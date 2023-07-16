72c5eb25f8ed4054419462b394245d27dd868a5c
Đinh Tiến Công <dinhtiencong113@gmail.com>
2023-07-16 19:09:54 +0700

    update dao
    Cong

--- admin/dao/product.php
+++ admin/dao/product.php
@@ -5,34 +5,34 @@ require_once 'pdo.php';
 
 class Product {
     public function getAllProducts() {
-        $sql = "SELECT * FROM products";
+        $sql = "SELECT * FROM product";
         $products = pdo_query($sql);
         return $products;
     }
 
     public function getProductById($productId) {
-        $sql = "SELECT * FROM products WHERE id = $productId";
+        $sql = "SELECT * FROM product WHERE id = $productId";
         $product = pdo_query_one($sql);
         return $product;
     }
 
     public function addProduct($name, $price, $description) {
-        $sql = "INSERT INTO products (name, price, description) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO product (name, price, description) VALUES (?, ?, ?)";
         pdo_execute($sql, $name, $price, $description);
     }
 
     public function updateProduct($productId, $name, $price, $description) {
-        $sql = "UPDATE products SET name = ?, price = ?, description = ? WHERE id = ?";
+        $sql = "UPDATE product SET name = ?, price = ?, description = ? WHERE id = ?";
         pdo_execute($sql, $name, $price, $description, $productId);
     }
 
     public function deleteProduct($productId) {
-        $sql = "DELETE FROM products WHERE id = ?";
+        $sql = "DELETE FROM product WHERE id = ?";
         pdo_execute($sql, $productId);
     }
 
     public function searchProducts($keyword) {
-        $sql = "SELECT * FROM products WHERE name LIKE ?";
+        $sql = "SELECT * FROM product WHERE name LIKE ?";
         $products = pdo_query($sql, '%' . $keyword . '%');
         return $products;
     }
--- admin/dao/user.php
+++ admin/dao/user.php
@@ -4,33 +4,33 @@ require_once 'pdo.php';
 
 class User {
     public function getAllUsers() {
-        $sql = "SELECT * FROM users";
+        $sql = "SELECT * FROM user";
         $users = pdo_query($sql);
         return $users;
     }
 
     public function getUserById($userId) {
-        $sql = "SELECT * FROM users WHERE id = ?";
+        $sql = "SELECT * FROM user WHERE id = ?";
         $user = pdo_query_one($sql, $userId);
         return $user;
     }
 
     public function addUser($name, $email, $password) {
-        $sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO user (name, email, password) VALUES (?, ?, ?)";
         pdo_execute($sql, $name, $email, $password);
     }
 
     public function updateUser($userId, $name, $email, $password) {
-        $sql = "UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?";
+        $sql = "UPDATE user SET name = ?, email = ?, password = ? WHERE id = ?";
         pdo_execute($sql, $name, $email, $password, $userId);
     }
 
     public function deleteUser($userId) {
-        $sql = "DELETE FROM users WHERE id = ?";
+        $sql = "DELETE FROM user WHERE id = ?";
         pdo_execute($sql, $userId);
     }
     public function searchUsers($keyword) {
-        $sql = "SELECT * FROM users WHERE name LIKE ?";
+        $sql = "SELECT * FROM user WHERE name LIKE ?";
         $users = pdo_query($sql, '%' . $keyword . '%');
         return $users;
     }
