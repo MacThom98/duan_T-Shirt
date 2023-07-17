6131e4731a467afa85113c81f0ce3f82b9a8a040
Đinh Tiến Công <dinhtiencong113@gmail.com>
2023-07-17 10:23:19 +0700

    update site + dtb
    Cong

--- global.php
+++ global.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Định nghĩa các url cần thiết được sử dụng trong website
-$ROOT_URL = "/duan_T-Shirt"; //đường dẫn gốc của website
+$ROOT_URL = "/duan_T-shirt"; //đường dẫn gốc của website
 $CONTENT_ADMIN_URL = "$ROOT_URL/public/admin"; //đường dẫn chứa tài nguyên tĩnh của admin(img, css, js)
 $CONTENT_SITE_URL = "$ROOT_URL/public/site"; //đường dẫn chứa tài nguyên tĩnh của site(img, css, js)
 $ADMIN_URL = "$ROOT_URL/admin"; //đường dẫn vào phần quản trị
@@ -10,7 +10,7 @@ $SITE_URL = "$ROOT_URL/site"; //đường dẫn vào phần site
 $PER_PAGE = 10; //số lượng hàng hóa mỗi trang
 
 // đường dẫn chứa hình khi upload
-$IMAGE_DIR = $_SERVER["DOCUMENT_ROOT"] . "$CONTENT_ADMIN_URL/images";
+$IMAGE_DIR = "$ROOT_URL/upload";
 
 // 2 biến toàn cục để chia sẻ giữa controller và view
 $VIEW_NAME = "index.php";
---
+++ site/model/pdo.php
@@ -0,0 +1,82 @@
+<?php
+// phương thức kết nối dữ liệu bằng PDO
+function pdo_get_connection(){
+    $dburl = "mysql:host=localhost;dbname=t-shirt;port=3307;charset=utf8";
+    $username = 'root';
+    $password = '';
+
+    $conn = new PDO($dburl, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+}
+
+
+// Phương thức thực thi các câu lệnh SQL như Thêm, xóa, sửa
+function pdo_execute($sql){
+    $sql_args = array_slice(func_get_args(), 1);
+    try{
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+
+// Phương thức select toàn bộ dữ liệu, sử dụng fetchAll()
+function pdo_query($sql){
+    $sql_args = array_slice(func_get_args(), 1);
+    try{
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $rows = $stmt->fetchAll();
+        return $rows;
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+// Sử dụng fetch(PDO::FETCH_ASSOC)  để lấy 1 dòng dữ liệu
+function pdo_query_one($sql){
+    $sql_args = array_slice(func_get_args(), 1);
+    try{
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+
+function pdo_query_value($sql){
+    $sql_args = array_slice(func_get_args(), 1);
+    try{
+        $conn = pdo_get_connection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        return array_values($row)[0];
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+
+?>
